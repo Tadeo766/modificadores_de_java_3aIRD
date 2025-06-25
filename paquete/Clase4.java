fa1224eecd5103769023d8cc416654245d267029
Tadeo766 <tadeodefermin766@gmail.com>
2025-06-25 12:54:31 -0600

    Adding Clase4

---
+++ paquete/Clase4.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Clase4 {
+    private boolean x;
+
+    public boolean isX(){
+        return x;
+    }
+
+    public void setX(boolean x){
+        this.x = x;
+    }
+    
+}
